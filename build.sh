7ecb6ce8909ee788f12eacf139070f080df645f9
mhkim <mhkim@cantops.biz>
2022-12-14 16:45:14 +0900

    vcm build.sh fix

--- build.sh
+++ build.sh
@@ -25,10 +25,23 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
+if [ ! -d ${BASEDIR}/vcm/build ]; then
+	    mkdir ${BASEDIR}/vcm/build
+fi
+cd ${BASEDIR}/vcm/build
+cmake ..
+make
+if [ $? != 0 ]; then
+	    echo "vcm compile error"
+	        exit 1
+fi
+
+
 cd ${BASEDIR}
 rm -rf ${BASEDIR}/release
 cp -R ${BASEDIR}/dist ${BASEDIR}/release
 cp ${BASEDIR}/ord/build/ord ${BASEDIR}/release/pim/opt/pim/bin/
 cp ${BASEDIR}/vsd/build/vsd ${BASEDIR}/release/pim/opt/pim/bin/
+cp ${BASEDIR}/vcm/build/vcm ${BASEDIR}/release/pim/opt/pim/bin/
 cd ${BASEDIR}/release
 dpkg -b pim
