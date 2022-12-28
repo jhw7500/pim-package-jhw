09d721ad433644b0394296c852120da5b7e84bf9
root <mhkim@cantops.biz>
2022-12-28 19:02:32 +0900

    pim_update example add

--- dist/pim/opt/pim/bin/pim_update.sh
+++ dist/pim/opt/pim/bin/pim_update.sh
@@ -1,5 +1,14 @@
-#extract tar file
+#!/bin/bash
 
 #update excute
+
+dpkg -i pim.deb
+
+if [ $? -eq 0 ];then
+	echo "dpkg install success!"
+else
+	echo "dpkg install Failure!"
+	exit 1
+fi
 
 exit 0;
