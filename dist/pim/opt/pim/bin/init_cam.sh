4e73247dbe986abe52b24556ed6e0dbbccfd2a88
hwjo <hwjo@cantops.biz>
2023-03-13 13:20:22 +0900

    new file

---
+++ dist/pim/opt/pim/bin/init_cam.sh
@@ -0,0 +1,15 @@
+pkill -9 streamApp
+pkill -9 PIMCAM
+sleep 2
+modprobe -r imx8-media-dev
+sleep 2
+modprobe -r max9296
+#rmmod imx8-media-dev
+#rmmod max9296
+
+sleep 1
+modprobe max9296
+sleep 1
+modprobe imx8-media-dev
+sleep 5
+PIMCAM -j /root/shared_v/edgeconf_pim.json &
