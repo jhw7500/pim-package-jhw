3211e3addd6a8506e785b6129a0412a81adfe843
hwjo <hwjo@cantops.biz>
2023-03-14 17:12:40 +0900

    ver 0.1.6

--- dist/pim/opt/pim/bin/init_cam.sh
+++ dist/pim/opt/pim/bin/init_cam.sh
@@ -1,3 +1,5 @@
+pkill -9 vcm
+pkill -9 ord
 pkill -9 streamApp
 pkill -9 PIMCAM
 sleep 2
@@ -13,3 +15,5 @@ sleep 1
 modprobe imx8-media-dev
 sleep 5
 PIMCAM -j /root/shared_v/edgeconf_pim.json &
+/opt/pim/bin/vcm &
+/opt/pim/bin/ord &
