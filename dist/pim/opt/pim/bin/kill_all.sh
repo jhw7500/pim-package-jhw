1433ae702ef7a7f0b95ff715370e23eb45c66858
hwjo <hwjo@cantops.biz>
2023-03-27 14:29:27 +0900

    kill_all.sh

--- dist/pim/opt/pim/bin/kill_all.sh
+++ dist/pim/opt/pim/bin/kill_all.sh
@@ -28,8 +28,28 @@ if [ ! -z "$service" ]; then
         fi
 fi
 
-sleep 1
+service=ord
+status=0
+if [ ! -z "$service" ]; then
+        pgrep "$service" >/dev/null; status=$?
+        if [ "$status" -eq 0 ]; then
+                sudo killall $service
+        fi
+fi
+
+service=vsd
+status=0
+if [ ! -z "$service" ]; then
+        pgrep "$service" >/dev/null; status=$?
+        if [ "$status" -eq 0 ]; then
+                sudo killall $service
+        fi
+fi
+
+#sleep 1
 /opt/pim/bin/vcm &
+/opt/pim/bin/ord &
+/opt/pim/bin/vsd &
 
 exit "$status"
 
