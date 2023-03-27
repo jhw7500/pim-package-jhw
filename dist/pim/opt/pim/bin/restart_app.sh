cb6ca4f3e4423a76288882073506bee4bc2fba40
hwjo <hwjo@cantops.biz>
2023-03-27 11:21:31 +0900

    0.1.8

---
+++ dist/pim/opt/pim/bin/restart_app.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+service=$1
+status=0
+status2=0
+while [ 1 ]; do 
+	if [ ! -z "$service" ]; then
+		pgrep "$service" >/dev/null; status=$?
+		if [ "$status" -eq 0 ]; then
+			echo "ok" > /dev/null
+		else
+			while [ 1 ]; do 
+				pgrep streamApp >/dev/null; status2=$?
+				if [ "$status2" -eq 0 ]; then
+					echo "Not yet" > /dev/null
+				else
+					break
+				fi
+				sleep 1
+			done
+			/usr/bin/PIMCAM -j /root/shared_v/edgeconf_pim.json &
+		fi
+	fi
+	sleep 1;
+done
+
+exit "$status"
+
