39cae8c4ec0ed84aa675b3c74ccf4d0481fdfbfc
mhkim <mhkim@cantops.biz>
2023-04-04 10:01:18 +0900

    cam connect check fix

--- dist/pim/opt/pim/bin/chk_cam_connect_temp.sh
+++ dist/pim/opt/pim/bin/chk_cam_connect_temp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 TEST_CONFIG_FILE="/root/shared_v/edgeconf_pim.json"
+FLAG_PATH="/opt/pim/bin/bg_chk_flag"
+LOG_PATH="/var/log"
 SUCCESS_VAL="0xfa"
 CAM0_ERR="0xda"
 CAM1_ERR="0xea"
@@ -7,7 +9,11 @@ CAM01_ERR="0x16"
 CAM2_ERR="0xda"
 CAM3_ERR="0xea"
 CAM23_ERR="0x16"
+SUCCESS_VAL="true"
+FAIL_VAL="error"
+result=0;
 timestamp=`date +"%Y-%m-%d %T,%3N"`
+
 
 
 #ch0/1 Des check
@@ -15,50 +21,57 @@ cam01_res=$(i2ctransfer -f -y -a 2 w2@0x48 0x00 0x13 r1)
 #ch2/3 Des check
 cam23_res=$(i2ctransfer -f -y -a 1 w2@0x48 0x00 0x13 r1)
 
-if [[ "$cam01_res" == *"$SUCCESS_VAL"*  ]]; then
-        echo "cam01 ok"
+
+if [[ ! -s "$TEST_CONFIG_FILE" ]]; then 
+	LogWrite "can't find $TEST_CONFIG_FILE"
+	result=1 ; 
 else
-        echo "cam01 error"
-        #echo "I2cwrite 0x90 0x0010 0x31"
-        i2ctransfer -f -y -a 2 w3@0x48 0x00 0x10 0x31
-        sleep 3
 
-        #echo "SER0 chk I2cwrite 0x80 0x0010 0x21"
-        i2ctransfer -f -y -a 2 w3@0x40 0x00 0x10 0x21
-        sleep 3
+	if [[ "$cam01_res" == *"$SUCCESS_VAL"*  ]]; then
+			echo "cam01 ok"
+	else
+			echo "cam01 error"
+			#echo "I2cwrite 0x90 0x0010 0x31"
+			i2ctransfer -f -y -a 2 w3@0x48 0x00 0x10 0x31
+			sleep 3
 
-        if [[ "$cam01_res" == *"$CAM1_ERR"* ]]; then
-                echo "CAM1_ERR : $cam01_res"
-        fi
+			#echo "SER0 chk I2cwrite 0x80 0x0010 0x21"
+			i2ctransfer -f -y -a 2 w3@0x40 0x00 0x10 0x21
+			sleep 3
 
-        if [[ "$cam01_res" == *"$CAM0_ERR"* ]]; then
-                echo "CAM0_ERR : $cam01_res"
-        fi
+			if [[ "$cam01_res" == *"$CAM1_ERR"* ]]; then
+					echo "CAM1_ERR : $cam01_res"
+			fi
 
-        if [[ "$cam01_res" == *"$CAM01_ERR"* ]]; then
-                echo "CAM01_ERR : $cam01_res"
-        fi
-fi
+			if [[ "$cam01_res" == *"$CAM0_ERR"* ]]; then
+					echo "CAM0_ERR : $cam01_res"
+			fi
 
-if [[ "$cam23_res" == *"$SUCCESS_VAL"*  ]]; then
-        echo "cam23 ok"
-else
-        echo "cam23 error"
-        #echo "I2cwrite 0x90 0x0010 0x31"
-        i2ctransfer -f -y -a 1 w3@0x48 0x00 0x10 0x31
-        sleep 3
-        #echo "SER1 chk I2cwrite 0x80 0x0010 0x21"
-        i2ctransfer -f -y -a 1 w3@0x40 0x00 0x10 0x21
-        sleep 3
-        if [[ "$cam23_res" == *"$CAM3_ERR"* ]]; then
-                echo "CAM3_ERR : $cam23_res"
-        fi
+			if [[ "$cam01_res" == *"$CAM01_ERR"* ]]; then
+					echo "CAM01_ERR : $cam01_res"
+			fi
+	fi
 
-        if [[ "$cam23_res" == *"$CAM2_ERR"* ]]; then
-                echo "CAM2_ERR : $cam23_res"
-        fi
+	if [[ "$cam23_res" == *"$SUCCESS_VAL"*  ]]; then
+			echo "cam23 ok"
+	else
+			echo "cam23 error"
+			#echo "I2cwrite 0x90 0x0010 0x31"
+			i2ctransfer -f -y -a 1 w3@0x48 0x00 0x10 0x31
+			sleep 3
+			#echo "SER1 chk I2cwrite 0x80 0x0010 0x21"
+			i2ctransfer -f -y -a 1 w3@0x40 0x00 0x10 0x21
+			sleep 3
+			if [[ "$cam23_res" == *"$CAM3_ERR"* ]]; then
+					echo "CAM3_ERR : $cam23_res"
+			fi
 
-        if [[ "$cam23_res" == *"$CAM23_ERR"* ]]; then
-                echo "CAM23_ERR : $cam23_res"
-        fi
+			if [[ "$cam23_res" == *"$CAM2_ERR"* ]]; then
+					echo "CAM2_ERR : $cam23_res"
+			fi
+
+			if [[ "$cam23_res" == *"$CAM23_ERR"* ]]; then
+					echo "CAM23_ERR : $cam23_res"
+			fi
+	fi
 fi
