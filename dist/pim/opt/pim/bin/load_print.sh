8edb7a10fb1f85d85edce6f2faeb263095d6d2fb
mhkim <mhkim@cantops.biz>
2023-04-04 10:03:08 +0900

    cpu, ram usage print add

---
+++ dist/pim/opt/pim/bin/load_print.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+function load_print() { 
+        _cpu_load=`top -b -d 0 -n2 | grep -Po '[0-9.]+ id' | awk '{print 100-$1}' | tail -n 1` 
+        mem_tot=`cat /proc/meminfo | grep ^MemTotal| grep -Po '[0-9.]+ kB' | awk '{print $1}'` 
+        mem_avail=`cat /proc/meminfo | grep ^MemAvailable | grep -Po '[0-9.]+ kB' | awk '{print $1}'` 
+        _ram_load=`echo "$mem_tot $mem_avail"|awk '{printf "%.1f", ($1 - $2)/$1*100}'` 
+        echo -e "cpu_load:$_cpu_load\tram_load:$_ram_load" 
+}
+
+while : 
+do 
+	load_print
+	sleep 1
+done
