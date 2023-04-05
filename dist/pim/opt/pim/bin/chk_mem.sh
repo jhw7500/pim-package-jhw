395fa3fb925a379cc8c9248c53fd7698234c3b2b
hwjo <hwjo@cantops.biz>
2023-04-05 16:11:35 +0900

    auto script test

--- dist/pim/opt/pim/bin/chk_mem.sh
+++ dist/pim/opt/pim/bin/chk_mem.sh
@@ -2,6 +2,7 @@
 
 #service=$1
 #logger -s -t 'sh   ' streamApp PIMCAM vcm 
+group="streamApp ord vcm vsd"
 status=0
 while :
 do
@@ -10,6 +11,7 @@ cpu=$(mpstat |tail -1 | awk '{print 100-$NF}')
 mem=$(sar -r 0 |tail -1 | awk '{print $5}')
 logger -p local0.notice -t $service [CPU] total cpu ${cpu}% memory ${mem}%
 
+for service in $group; do
 if [ ! -z "$service" ]; then
 	pgrep "$service" >/dev/null; status=$?
 	if [ "$status" -eq 0 ]; then
@@ -19,38 +21,8 @@ if [ ! -z "$service" ]; then
 		logger -p local0.info -t $service [MEM] ${mem}
 	fi
 fi
-service=vcm
-if [ ! -z "$service" ]; then
-        pgrep "$service" >/dev/null; status=$?
-        if [ "$status" -eq 0 ]; then
-                pid=$(ps -C $service |grep $service |awk '{print $1}')
-                #echo $service" pid:":${pid}
-                mem=$(pmap -x $pid |tail -1 |awk '{print "Kbytes:"$3" RSS:"$4" Dirty:"$5}')
-		logger -p local0.info -t $service [MEM] ${mem}
-        fi
-fi
-service=ord
-if [ ! -z "$service" ]; then
-        pgrep "$service" >/dev/null; status=$?
-        if [ "$status" -eq 0 ]; then
-                pid=$(ps -C $service |grep $service |awk '{print $1}')
-                #echo $service" pid:":${pid}
-                mem=$(pmap -x $pid |tail -1 |awk '{print "Kbytes:"$3" RSS:"$4" Dirty:"$5}')
-		logger -p local0.info -t $service [MEM] ${mem}
-        fi
-fi
+done
 
-service=vsd
-if [ ! -z "$service" ]; then
-        pgrep "$service" >/dev/null; status=$?
-        if [ "$status" -eq 0 ]; then
-                pid=$(ps -C $service |grep $service |awk '{print $1}')
-                #echo $service" pid:":${pid}
-                mem=$(pmap -x $pid |tail -1 |awk '{print "Kbytes:"$3" RSS:"$4" Dirty:"$5}')
-		logger -p local0.info -t $service [MEM] ${mem}
-        fi
-fi
-
-sleep 60
+sleep 1
 done
 
