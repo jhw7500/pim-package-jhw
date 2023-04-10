4f9d24f5513fb166b76aef2c78a47a1608594dd2
hwjo <hwjo@cantops.biz>
2023-04-10 16:13:52 +0900

    bg chk cmd change

--- dist/pim/opt/pim/bin/BG_Check_for_pim.sh
+++ dist/pim/opt/pim/bin/BG_Check_for_pim.sh
@@ -5,8 +5,48 @@ function CLEAR_CHK_LOG() {
     rm ${FLAG_PATH}/*.*
 }
 
+
+function MAKE_RESULT_FLAG() {
+	result=0
+	if [[ -e ${FLAG_PATH}/err_cam0.log ]]; then 
+		result=$(echo "$result + 1" | bc)
+		echo "bg_chk_flag : $result"
+	fi
+	if [[ -e ${FLAG_PATH}/err_cam1.log ]]; then 
+		result=$(echo "$result + 2" | bc)
+		echo "bg_chk_flag : $result"
+	fi	
+	if [[ -e ${FLAG_PATH}/err_cam2.log ]]; then 
+		result=$(echo "$result + 4" | bc)
+		echo "bg_chk_flag : $result"
+	fi		
+	if [[ -e ${FLAG_PATH}/err_cam3.log ]]; then 
+		result=$(echo "$result + 8" | bc)
+		echo "bg_chk_flag : $result"
+	fi		
+	if [[ -e ${FLAG_PATH}/err_wifi.log ]]; then 
+		result=$(echo "$result + 16" | bc)
+		echo "bg_chk_flag : $result"
+	fi		
+	if [[ -e ${FLAG_PATH}/err_sdcard.log ]]; then 
+		result=$(echo "$result + 32" | bc)
+		echo "bg_chk_flag : $result"
+	fi		
+	if [[ -e ${FLAG_PATH}/err_cpu_temp.log ]]; then 
+		result=$(echo "$result + 64" | bc)
+		echo "bg_chk_flag : $result"
+	fi		
+	if [[ -e ${FLAG_PATH}/err_voltage.log ]]; then 
+		result=$(echo "$result + 128" | bc)
+		echo "bg_chk_flag : $result"
+	fi
+	
+	printf "0x%x" $result > ${FLAG_PATH}/bg_chk_flag.bin
+}
+
 CLEAR_CHK_LOG
 while true; do
+	sleep 10
     #cam connect check
     /opt/pim/bin/chk_cam_connect.sh
     #wifi check
@@ -19,6 +59,6 @@ while true; do
     /opt/pim/bin/chk_cpu_temp.sh
     #power check
     /opt/pim/bin/chk_voltage.sh
-
-    sleep 10
+    #make result cmd
+    MAKE_RESULT_FLAG
 done
