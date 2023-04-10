c92f61fc269d80ceed671494ada0deb7b062475e
hwjo <hwjo@cantops.biz>
2023-04-10 13:19:25 +0900

    logger

--- dist/pim/opt/pim/bin/chk_mem.sh
+++ dist/pim/opt/pim/bin/chk_mem.sh
@@ -4,12 +4,14 @@
 #logger -s -t 'sh   ' streamApp PIMCAM vcm 
 group="streamApp ord vcm vsd"
 status=0
+tag=$(basename "$0")
+
 while :
 do
-service=streamApp
+#service=streamApp
 cpu=$(mpstat |tail -1 | awk '{print 100-$NF}')
 mem=$(sar -r 0 |tail -1 | awk '{print $5}')
-logger -p local0.notice -t $service [CPU] total cpu ${cpu}% memory ${mem}%
+logger -s -p local0.notice -t $tag [CHK] total cpu ${cpu}% memory ${mem}%
 
 for service in $group; do
 if [ ! -z "$service" ]; then
@@ -18,7 +20,7 @@ if [ ! -z "$service" ]; then
 		pid=$(ps -C $service |grep $service |awk '{print $1}')		
 		#echo $service" pid:":${pid}
 		mem=$(pmap -x $pid |tail -1 |awk '{print "Kbytes:"$3" RSS:"$4" Dirty:"$5}')
-		logger -p local0.info -t $service [MEM] ${mem}
+		logger -p local0.info -t $tag [CHK] $service ${mem}
 	fi
 fi
 done
