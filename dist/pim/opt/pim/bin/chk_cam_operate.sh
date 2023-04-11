6d526293cfc32954dd835fcb11121e29ec03e4ba
hwjo <hwjo@cantops.biz>
2023-04-11 17:23:11 +0900

    cam-operate daemon

---
+++ dist/pim/opt/pim/bin/chk_cam_operate.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+FILE_=/tmp/start_video_time_
+FILE_JSON=/root/shared_v/edgeconf_pim.json
+FILE_JSON_=/root/shared_v/ord_vcm_conf.json
+tag=$(basename "$0")
+ENABLE_VAL=true
+DISABLE_VAL=false
+check_num=0
+while :
+do
+	startTime=$(cat $FILE_ | tr -d '\n')
+	if [ -n "$startTime"  ]; then
+		curTimeEpoch=$(date "+%s")
+		logger -p local0.notice -t $tag [CHK] start_video_time_ : $startTime
+		logger -p local0.notice -t $tag [CHK] cur_time : $(date "+%Y%m%d %H:%M:%S")
+		#echo $(date "+%Y%m%d %H:%M:%S")
+
+		startTimeEpoch=$(date -d "$startTime" "+%s")
+		#echo $curTimeEpoch
+		#echo $startTimeEpoch
+		diffEpoch=$(echo "$curTimeEpoch - $startTimeEpoch" |bc)
+		logger -p local0.notice -t $tag [CHK] diffEpoch : $diffEpoch
+		if [ "$diffEpoch" -ge 10 ]; then
+			cat /dev/null > $FILE_
+			cam_ch0_en=$(cat $FILE_JSON | grep cam_ch0 | grep -v rotate | cut -d':' -f2 | cut -d',' -f1 | tr -d '"' | tr -d '\r\n')
+			cam_ch1_en=$(cat $FILE_JSON | grep cam_ch1 | grep -v rotate | cut -d':' -f2 | cut -d',' -f1 | tr -d '"' | tr -d '\r\n')
+			cam_ch2_en=$(cat $FILE_JSON | grep cam_ch2 | grep -v rotate | cut -d':' -f2 | cut -d',' -f1 | tr -d '"' | tr -d '\r\n')
+			cam_ch3_en=$(cat $FILE_JSON | grep cam_ch3 | grep -v rotate | cut -d':' -f2 | cut -d',' -f1 | tr -d '"' | tr -d '\r\n')
+			mp4date=$(date -d "$startTime" "+%Y%m%d_%H%M%S")
+			#ch0_file=$(find /mnt -name *"$mp4date"-ch0.mp4)
+			#echo $ch0_file
+			#list="cam_ch0_en cam_ch1_en"
+			#for ch in $list; do
+			#echo $ch
+			if [[ "$cam_ch0_en" == *"$ENABLE_VAL"* ]]; then
+				if [ -f /mnt/*"$mp4date"-ch0.mp4 ]; then
+					logger -p local0.info -t $tag [CHK] *"$mp4date"-ch0.mp4 exist
+				else
+					logger -p local0.error -t $tag [CHK]*"$mp4date"-ch0.mp4 not exist
+				fi
+				((check_num++))
+			fi
+			#done
+                        if [[ "$cam_ch1_en" == *"$ENABLE_VAL"* ]]; then
+                                if [ -f /mnt/*"$mp4date"-ch1.mp4 ]; then
+                                        logger -p local0.info -t $tag [CHK] *"$mp4date"-ch1.mp4 exist
+                                else
+                                        logger -p local0.error -t $tag [CHK] *"$mp4date"-ch1.mp4 exist
+                                fi
+				((check_num++))
+                        fi
+                        if [[ "$cam_ch2_en" == *"$ENABLE_VAL"* ]]; then
+                                if [ -f /mnt/*"$mp4date"-ch2.mp4 ]; then
+                                        logger -p local0.info -t $tag [CHK] *"$mp4date"-ch2.mp4 exist
+                                else
+                                        logger -p local0.error -t $tag [CHK] *"$mp4date"-ch2.mp4 not exist
+                                fi
+				((check_num++))
+                        fi
+                        if [[ "$cam_ch3_en" == *"$ENABLE_VAL"* ]]; then
+                                if [ -f /mnt/*"$mp4date"-ch3.mp4 ]; then
+                                        logger -p local0.info -t $tag [CHK] *"$mp4date"-ch3.mp4 exist
+                                else
+                                        logger -p local0.error -t $tag [CHK] *"$mp4date"-ch3.mp4 not exist
+                                fi
+				((check_num++))
+                        fi
+			srt_en=$(cat $FILE_JSON_ | grep srt_enable | cut -d':' -f2 | cut -d',' -f1 | tr -d '"' | tr -d '\r\n')
+                        if [[ "$srt_en" == *"$ENABLE_VAL"* ]]; then
+                                if [ -f /mnt/*"$mp4date"-data.srt ]; then
+                                        logger -p local0.info -t $tag [CHK] *"$mp4date"-data.srt exist
+                                else
+                                        logger -p local0.error -t $tag [CHK] *"$mp4date"-data.srt exist
+                                fi
+				((check_num++))
+                        fi
+
+			#echo $check_num
+			#echo $ch0_file
+			#echo $mp4date
+			filecnt=$(ls -l /mnt/*"$mp4date"* |grep ^- |wc -l)
+			#echo $filecnt
+			if [ "$check_num" -gt "$filecnt" ]; then
+				logger -p local0.error -t $tag [CHK] mp4,srt file cnt check fail
+				/opt/pim/bin/kill_test.sh
+			else
+				logger -p local0.notice -t $tag [CHK] mp4.srt file cnt check ok
+			fi
+		fi
+	fi
+	sleep 5
+done
