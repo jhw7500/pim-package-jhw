f6ebffafe9ed5ca547cbd9cb1b28c264c3377f2d
hwjo <hwjo@cantops.biz>
2023-04-17 17:33:56 +0900

    max9296 log, syslog, ord, vcm..

--- dist/pim/opt/pim/bin/kill_all.sh
+++ dist/pim/opt/pim/bin/kill_all.sh
@@ -4,20 +4,28 @@ tag=$(basename "$0")
 logger -p local0.notice [CHK][$tag:$LINENO] kill_all start
 list="BG_Check_for_pim.sh restart_app.sh vcm ord vsd streamApp PIMCAM"
 #list="chk_mem.sh"
-
+limitcnt=15
+key=CHK
 for service in $list; do
 if [ ! -z "$service" ]; then
+	cnt=0
         while :
         do
 		sudo killall -s KILL $service
 	        pgrep "$service" >/dev/null; status=$?
 	        if [ "$status" -eq 0 ]; then
-                	#pid=$(ps -C $service |grep $service |awk '{print $1}')
-                	#echo $service" pid:":${pid}
-                	logger -s -p local0.notice [CHK][$tag:$LINENO] wait for $service kill...
-                	#sudo kill -9 $pid
-               		#sudo killall -s KILL $service
-			sleep 1
+			if [ "$cnt" -ge "$limitcnt" ]; then
+				logger -s -p local0.notice [$key][$tag:$LINENO] $limitcnt sec over! 
+				break
+			else
+                		#pid=$(ps -C $service |grep $service |awk '{print $1}')
+                		#echo $service" pid:":${pid}
+                		logger -s -p local0.notice [$key][$tag:$LINENO] $cnt wait for $service kill...
+                		#sudo kill -9 $pid
+               			#sudo killall -s KILL $service
+				sleep 1
+				((cnt++))
+			fi
 		else
 			break
         	fi
