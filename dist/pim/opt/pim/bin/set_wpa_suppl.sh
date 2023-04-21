5e501f1a20ab5545150b4d9ee2c2c7f1c1d91984
mhkim <mhkim@cantops.biz>
2023-04-21 15:07:13 +0900

    set wpa supplicant log add

--- dist/pim/opt/pim/bin/set_wpa_suppl.sh
+++ dist/pim/opt/pim/bin/set_wpa_suppl.sh
@@ -4,11 +4,11 @@
 for ((var=0 ; var < 5; var++)); do
 	/sbin/wpa_supplicant -c /etc/wpa_supplicant/wpa_supplicant.conf -iwlp1s0 -Dnl80211,wext -B
 	if [ $? -eq 0 ];then
-	  echo "set_wpa_supplicant success"
+	  logger -p local0.notice [WPA][$tag:$LINENO] set_wpa_supplicant chmask success
 	  exit 0
 	else
-	  echo "set wpa_supplicant fail try again"	
+	  logger -p local0.notice [WPA][$tag:$LINENO] set_wpa_supplicant try again
 	fi
 done
-echo "set_wpa_supplicant fail"
+logger -p local0.error [WPA][$tag:$LINENO] set_wpa_supplicant chmask fail
 exit 1
