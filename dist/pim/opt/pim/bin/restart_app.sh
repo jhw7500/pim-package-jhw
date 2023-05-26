9ee93a98175ebedc310680d4d8a01b8622c2cc31
hwjo <hwjo@cantops.biz>
2023-05-26 09:46:28 +0900

    version 0.3.2

--- dist/pim/opt/pim/bin/restart_app.sh
+++ dist/pim/opt/pim/bin/restart_app.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-service=PIMCAM
-status=0
-status2=0
 tag=$(basename "$0")
 KEY=RST
 
@@ -10,33 +7,22 @@ KEY=RST
 list="vsd ord vcm"
 
 while [ 1 ]; do
-	if [ ! -z "$service" ]; then
-		pgrep "$service" >/dev/null; status=$?
-		if [ "$status" -eq 0 ]; then
-			echo "ok" > /dev/null
-		else
-			while [ 1 ]; do 
-				pgrep streamApp >/dev/null; status2=$?
-				if [ "$status2" -eq 0 ]; then
-					#echo "Not yet" > /dev/null
-					logger -p local0.notice [$KEY][$tag:$LINENO] streamApp exit not yet
-				else
-					pgrep BG_Check_for_pim.sh >/dev/null; status2=$?
-					if [ "$status2" -eq 0 ]; then
-						logger -p local0.notice [$KEY][$tag:$LINENO] BG_Check_for_pim not yet
-					else
-						break
-					fi
-				fi
-				sleep 1
-			done
-			logger -p local0.notice [$KEY][$tag:$LINENO] PIMCAM, streamApp, BG_Check_for_pim start 
-			#/opt/pim/bin/vcm &
-			/usr/bin/PIMCAM -j /root/shared_v/edgeconf_pim.json &
-			/opt/pim/bin/BG_Check_for_pim.sh & 2>/dev/null
-	                #systemctl restart cam-operate
-		fi
-	fi
+    pid=$(ps -ef |grep PIMCAM |grep -v grep |awk '{print $2}')
+    if [ -z "$pid" ]; then
+        logger -p local0.notice "[$KEY][$tag:$LINENO] PIMCAM killed"
+        pid=$(ps -ef |grep streamApp |grep -v grep |awk '{print $2}')
+        if [ -z "$pid" ]; then
+            logger -p local0.emerg "[$KEY][$tag:$LINENO] PIMCAM streamApp start"
+	        /usr/bin/PIMCAM -j /root/shared_v/edgeconf_pim.json &
+            pid=$(ps -ef |grep BG_Check_for_pim.sh |grep -v grep |awk '{print $2}')
+            if [ -z "$pid" ]; then
+                logger -p local0.emerg "[$KEY][$tag:$LINENO] BG_Check_for_pim.sh start"
+                /opt/pim/bin/BG_Check_for_pim.sh & 2>/dev/null
+            fi
+        else
+            logger -p local0.notice  "[$KEY][$tag:$LINENO] streamApp($pid) kill not yet"
+        fi
+    fi
 
     for service in $list; do
         if [ ! -z "$service" ]; then
@@ -53,7 +39,7 @@ while [ 1 ]; do
     done
 
 	sleep 1
-
 done
-exit "$status"
+
+exit 0
 
