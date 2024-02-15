74d96d5a40a778a8045c3cc45ae93b9694cc98d2
hwjo <hwjo@cantops.biz>
2024-02-15 12:51:07 +0900

    gstApp rtsp timestamp debug mode update_edgeconf optimize

--- dist/pim/opt/pim/bin/install_deb.sh
+++ dist/pim/opt/pim/bin/install_deb.sh
@@ -8,7 +8,9 @@ do
     fi
     sleep 1
 done
+
+update_edgeconf
 echo -e "\e[33mcomplete install_deb.sh\e[0m"
-echo -e "\e[33mif you want the streamApp, run 'update_edgeconf 1' but you want the gstApp, run 'update_edgeconf 2'\e[0m"
+echo -e "\e[33mif you want the streamApp, run 'update_edgeconf 1', but want the gstApp, run 'update_edgeconf 2'\e[0m"
 
 exit 0
