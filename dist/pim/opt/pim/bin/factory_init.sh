12b2d487b0f221e5033731d3499bb3fb472490c5
JungKil Park <jkpark@cantops.biz>
2024-05-24 16:44:36 +0900

    sdcard_fat32_format

--- dist/pim/opt/pim/bin/factory_init.sh
+++ dist/pim/opt/pim/bin/factory_init.sh
@@ -3,7 +3,7 @@
 tag=$(basename "$0")
 
 logger -p local0.notice [FIN][$tag:$LINENO] FACTORY INIT START
-#kill streamApp, PIMCAM, sd card format 
+#kill streamApp, PIMCAM, sd card format
 /opt/pim/bin/sdcard_fat32_format.sh
 
 #edgeconf_pim.json init
@@ -16,6 +16,9 @@ cp /opt/pim/config/ord_vcm_conf.json /root/shared_v/
 #network setting
 python3 /opt/cis/bin/init.py power_on
 python3 /opt/cis/bin/update_network.py
+
+#rootfs sizeup
+/opt/pim/bin/auto_fs_sizeup.sh 2 2
 
 #log init
 rm /var/log/cantops/*.*
