8c381eda5349705d098fbdb82667d66dbe4f338d
hwjo <hwjo@cantops.biz>
2024-10-22 10:53:22 +0900

    add chk_disk_io.sh

---
+++ dist/pim/opt/pim/bin/chk_disk_io.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+tag=$(basename "$0")
+KEY=DSK
+
+logger -p local0.notice "[$KEY][$tag:$LINENO] start"
+#LOGFILE="disk_io_monitor.log"
+INTERVAL=1
+
+while true; do
+    #iostat_output=$(iostat -x 1 2 | awk '$1 ~ /^[a-zA-Z0-9]/ && $NF ~ /^[0-9.]+$/ {print $1, $NF}')
+    #iostat_output=$(iostat -x 1 2 | awk '/^Device:/ {getline} {if ($1 != "" && $NF != "") print $1, $NF}')
+    #iostat_ouput=$(iostat -x 1 2 | awk '$1 ~ /^mmcblk/ {print $1, $NF}')
+    iostat_output=$(iostat -x 1 2 | awk 'NR>7' | awk '$1 ~ /^mmcblk/ {print $1, $NF}') 
+    echo "$iostat_output" | while read -r device util; do
+        if [[ "$util" =~ ^[0-9]+([.][0-9]+)?$ ]]; then
+            if (( $(echo "$util >= 100" | bc -l) )); then
+                #echo "$(date) - Disk I/O utilization for $device is $util%."
+                logger -p local0.warn "[$KEY][$tag:$LINENO] Disk I/O utilization for $device is $util%"
+            fi
+        fi
+    done
+    sleep $INTERVAL
+done
+
+logger -p local0.notice "[$KEY][$tag:$LINENO] exit"
