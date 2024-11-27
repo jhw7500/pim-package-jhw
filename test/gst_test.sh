a058a0abbca2472ba7909630728bfa9fe29bed2f
hwjo <hwjo@cantops.biz>
2024-11-27 13:18:04 +0900

    version 0.4.7 ord 4.1 vcm 3.9 pim_gate for nanya

--- test/gst_test.sh
+++ test/gst_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GST_DEBUG=3 ./bin/gstApp -e 0 -E 0 -N 1 -y 1 -C 1 -d 5 -c 0x3 -a 1 -f 1
+GST_DEBUG=2 GST_DEBUG_DUMP_DOT_DIR=/home/user/pim-package/test/ ./bin/gstApp -e 0 -E 0 -N 1 -y 1 -C 1 -d 3 -c 0x3 -a 1 -f 1
