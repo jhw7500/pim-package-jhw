ecaf44b89f5370dc4f8fa1441acfb1b32bdbf7e3
hwjo <hwjo@cantops.biz>
2025-07-15 10:16:02 +0900

    add capture timeout in edgeconf

--- dist/pim/opt/pim/bin/update_edgeconf.sh
+++ dist/pim/opt/pim/bin/update_edgeconf.sh
@@ -139,8 +139,9 @@ echo "check capture"
 #jq 'del (.VHL_CAM.capture)' "$FILE_JSON" > temp.json && mv temp.json "$FILE_JSON"
 #jq --argjson capture_en "$capture_en" '.VHL_CAM.capture |= (if .enable == null then .enable = $capture_en else . end)
 #| .VHL_CAM.capture |= (if .delay == null then .delay = 0 else . end)' "$FILE_JSON" > temp.json && mv temp.json "$FILE_JSON"
-jq --argjson key0 "$capture_en" --argjson key1 0 '.VHL_CAM.capture |= (if .enable == null then .enable = $key0 else . end)
-| .VHL_CAM.capture |= (if .delay == null then .delay = $key1 else . end)' "$FILE_JSON" > temp.json && mv temp.json "$FILE_JSON"
+jq --argjson key0 "$capture_en" --argjson key1 0 --argjson key2 5 '.VHL_CAM.capture |= (if .enable == null then .enable = $key0 else . end)
+| .VHL_CAM.capture |= (if .delay == null then .delay = $key1 else . end)
+| .VHL_CAM.capture |= (if .timeout == null then .timeout = $key2 else . end)' "$FILE_JSON" > temp.json && mv temp.json "$FILE_JSON"
 
 echo "update i2c header"
 jq '.VHL_CAM.i2c2 |= (if .exp_time == null then .exp_time = 10000 else . end)
