ef6e80765309212be913a089fae3c11e43514849
jkpark <jkpark@cantops.biz>
2025-09-23 08:56:29 +0900

    edit update_time_sync.sh

--- dist/pim/opt/pim/bin/update_time_sync.sh
+++ dist/pim/opt/pim/bin/update_time_sync.sh
@@ -10,25 +10,33 @@ if [ "$ntp_svr" == "null" ]; then
     ntp_svr=""
 fi
 
-a="/opt/pim/etc/systemd/timesyncd.conf" 
-b="/etc/systemd/timesyncd.conf"
-if ! cmp -s -- "$a" "$b" 2>/dev/null; then
-    cp /opt/pim/etc/systemd/timesyncd.conf /etc/systemd/timesyncd.conf
-fi
-
+dst="/etc/systemd/timesyncd.conf"
 if [ -z "$ntp_svr" ]; then
     #echo "disable ntp_server"
+    ori=/opt/pim/etc/systemd/timesyncd.conf
+    if ! cmp -s -- "$ori" "$dst" 2>/dev/null; then
+        cp $ori $dst
+    fi
     status=$(timedatectl |grep NTP|awk '{print $3}')
     if [ "$status" != "inactive" ]; then
       timedatectl set-ntp false
     fi
 else
     #echo "enable ntp_server"
-    sed -i "s/^#NTP=.*/NTP=$ntp_svr/" $b
-    sed -i 's/^#RootDistanceMaxSec=.*/RootDistanceMaxSec=10000000/' $b
-    sed -i 's/^#PollIntervalMinSec=.*/PollIntervalMinSec=32/' $b
-    sed -i 's/^#PollIntervalMaxSec=.*/PollIntervalMaxSec=2048/' $b
-
-    systemctl restart systemd-timesyncd
-    timedatectl set-ntp true
+    change_flag=0
+    temp="/tmp/timesyncd.conf"
+    cp /opt/pim/etc/systemd/timesyncd.conf $temp
+    sed -i "s/^#NTP=.*/NTP=$ntp_svr/" $temp
+    sed -i 's/^#RootDistanceMaxSec=.*/RootDistanceMaxSec=10000000/' $temp
+    sed -i 's/^#PollIntervalMinSec=.*/PollIntervalMinSec=32/' $temp
+    sed -i 's/^#PollIntervalMaxSec=.*/PollIntervalMaxSec=2048/' $temp
+    if ! cmp -s -- "$temp" "$dst" 2>/dev/null; then
+        cp $temp $dst
+        change_flag=1
+    fi
+    status=$(timedatectl |grep NTP|awk '{print $3}')
+    if [ "$change_flag" -ne 0 ] || [ "$status" == "inactive" ]; then
+        systemctl restart systemd-timesyncd
+        timedatectl set-ntp true
+    fi
 fi
