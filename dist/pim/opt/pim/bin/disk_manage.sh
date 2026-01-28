c3e1030431ca88a10ea0b21e88a08bcc7a019f7f
hwjo <hwjo@cantops.biz>
2026-01-28 16:03:44 +0900

    Deprecate disk_manage retention

--- dist/pim/opt/pim/bin/disk_manage.sh
+++ dist/pim/opt/pim/bin/disk_manage.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 tag=$(basename "$0")
 KEY=DSK
-logger -p local0.notice [$KEY][$tag:$LINENO] disk_manage.sh start target:$1 size:$2 cnt:$3
-mount /dev/mmcblk1p1 $1
-#echo disk_manage.sh start target:$1 size:${2} cnt:${3}
-#mnt=$(df -h |grep $1 |awk '{print $5}'|sed 's/[^0-9]//g')
-#mnt_s=$(df -h |grep /mnt |awk '{print $5}'|sed -r 's/^0+/g')
-#int=$((mnt))
-mnt=0
-use=0
-cnt=0
-
-while :
-do
-mnt=$(df -h |grep $1 |awk '{print $5}'|sed 's/[^0-9]//g')
-use=$((mnt))
-if [ $use -gt $2 ]; then
-    logger -p local0.notice "[$KEY][$tag:$LINENO] size $use% > $2%"
-    ls -pr /mnt/sd_cam | grep -v '/$' | tail -n 10 | xargs -t -I %% sh -c '{ rm -f /mnt/sd_cam/%%; }'
-fi
-cnt=$(ls -lt $1 | grep ^- | wc -l)
-if [ $cnt -gt $3 ]; then
-    logger -p local0.notice "[$KEY][$tag:$LINENO] cnt $cnt > $3"
-    ls -pr /mnt/sd_cam | grep -v '/$' | tail -n 10 | xargs -t -I %% sh -c '{ rm -f /mnt/sd_cam/%%; }'
-fi
-
-sleep 30
-done
+logger -p local0.warning "[$KEY][$tag:$LINENO] disk_manage.sh is deprecated; retention is handled by chk_cam_operate.sh (session-aware). No action taken."
+exit 0
