ccacdaedc1310fdbfe31be7864964d1b48ffa408
hwjo <hwjo@cantops.biz>
2026-02-10 18:07:16 +0900

    Runtime artifacts
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- dist/pim/opt/pim/bin/log_manage.sh
+++ dist/pim/opt/pim/bin/log_manage.sh
@@ -31,7 +31,7 @@ fi
 cnt=$(ls -lt $1/dot | grep ^- | wc -l)
 echo "dot:$cnt"
 if [ $cnt -gt $LIMIT ]; then
-    logger -p local0.notice "[$KEY][$tag:$LINENO] dot file cnt $cnt > $LIMIT ($tailcnt)"
+    logger -p local0.info "[$KEY][$tag:$LINENO] dot file cnt $cnt > $LIMIT ($tailcnt)"
     #ls -pr $INPUT_PATH | grep -v '/$' | tail -n 1 | xargs -t -I %% sh -c '{ rm -f /var/log/cantops/dot/%%; }'
     #find $INPUT_PATH/dot -maxdepth 1 -type f | sort -r | tail -$tailcnt| xargs rm -f
     find $INPUT_PATH/dot -maxdepth 1 -type f -printf '%T+ %p\n' | sort | head -n -$LIMIT | cut -d' ' -f2- | xargs -r rm -f
@@ -40,7 +40,7 @@ fi
 cnt=$(ls -lt $1/gst | grep ^- | wc -l)
 echo "gst:$cnt"
 if [ $cnt -gt $LIMIT ]; then
-    logger -p local0.notice "[$KEY][$tag:$LINENO] gst file cnt $cnt > $LIMIT ($tailcnt)"
+    logger -p local0.info "[$KEY][$tag:$LINENO] gst file cnt $cnt > $LIMIT ($tailcnt)"
     #ls -pr $INPUT_PATH | grep -v '/$' | tail -n 1 | xargs -t -I %% sh -c '{ rm -f /var/log/cantops/dot/%%; }'
     #find $INPUT_PATH/gst -maxdepth 1 -type f | sort -r | tail -$tailcnt| xargs rm -f
     find $INPUT_PATH/gst -maxdepth 1 -type f -printf '%T+ %p\n' | sort | head -n -$LIMIT | cut -d' ' -f2- | xargs -r rm -f
