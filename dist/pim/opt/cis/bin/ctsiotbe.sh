cadea904bfdbd43c300bb274259bc1dd4522583e
jkpark <jkpark@cantops.biz>
2026-02-12 14:38:38 +0900

    debug pim_gate and add vns db file

--- dist/pim/opt/cis/bin/ctsiotbe.sh
+++ dist/pim/opt/cis/bin/ctsiotbe.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 IMAGENAME="192.168.10.10:48136/ctsiotbe"
+
+# 환경변수 기본값 (256GB 디스크 기준)
+EVENT_STORAGE_LIMIT_MB=${EVENT_STORAGE_LIMIT_MB:-204800}
+EVENT_CLEANUP_BATCH_SIZE=${EVENT_CLEANUP_BATCH_SIZE:-10}
+
 start() {
     mkdir -p /mnt/sd_cam/event
     docker rm -f ctsiotbe 2>/dev/null
     docker run --rm -d --name=ctsiotbe \
     -p 5000:5000 \
+    -e EVENT_STORAGE_LIMIT_MB=${EVENT_STORAGE_LIMIT_MB} \
+    -e EVENT_CLEANUP_BATCH_SIZE=${EVENT_CLEANUP_BATCH_SIZE} \
     -v /dev/shm/be_share:/data/share \
     -v /mnt/sd_cam/event:/data/db \
     ${IMAGENAME}
