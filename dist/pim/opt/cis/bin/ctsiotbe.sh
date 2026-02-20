89b7d698673deff983f99b83944a18beaa5cb1cf
jkpark <jkpark@cantops.biz>
2026-02-20 15:51:46 +0900

    update, pim_gate

--- dist/pim/opt/cis/bin/ctsiotbe.sh
+++ dist/pim/opt/cis/bin/ctsiotbe.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 IMAGENAME="192.168.10.10:48136/ctsiotbe"
 
-# 환경변수 기본값 (256GB 디스크 기준)
-EVENT_STORAGE_LIMIT_MB=${EVENT_STORAGE_LIMIT_MB:-204800}
+EVENT_STORAGE_LIMIT_MB=$(python3 /opt/pim_gate/bin/v1/pimconf.py pimconf.record_event_max_mb | tr -d '\r\n')
 EVENT_CLEANUP_BATCH_SIZE=${EVENT_CLEANUP_BATCH_SIZE:-10}
 
 start() {
+    logger -p local2.info "CTSIOTBE|storage limit ${EVENT_STORAGE_LIMIT_MB} MB"
     mkdir -p /mnt/sd_cam/event
     docker rm -f ctsiotbe 2>/dev/null
     docker run --rm -d --name=ctsiotbe \
