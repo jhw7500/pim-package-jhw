fdcbea0a61a5a3f1d72a7012f7b61200d113c5db
hwjo <hwjo@cantops.biz>
2026-04-05 13:20:22 +0900

    ci: workflow 설정 파일 추가 (workflow-config, commitlint)

---
+++ .github/commitlint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always', [
+      'feat', 'fix', 'chore', 'refactor', 'docs',
+      'test', 'build', 'ci', 'perf', 'style'
+    ]],
+    // 한글 커밋 메시지 허용
+    'subject-case': [0],
+  },
+};
